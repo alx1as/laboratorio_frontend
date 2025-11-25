c95c886d59157b8c35664b5f1522c9b2aacf5a60
Alx1as <alexiarubin23@gmail.com>
2025-11-25 10:05:44 -0300

    api de backend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
- const API = "https://hartistashartantes-e4aebga5evd7dgbd.eastus2-01.azurewebsites.net/api";
+ const API = "https://bk-laboratorio-fvfzcngzajakcmbk.eastus2-01.azurewebsites.net/api";
 
 export async function apiGet(path) {
   const res = await fetch(`${API}${path}`, {
